369bb45eb0713e04b513ae62284729e8aa0361fe
lh <2928446205@qq.com>
2022-04-12 13:17:04 +0800

    test

--- DirList.java
+++ DirList.java
@@ -5,6 +5,7 @@ import java.io.File;
 public class DirList {
     public static void main(String args[]) {
         String dirname = "C:/Users/29284/IdeaProjects/Manager/src/";
+        System.out.println("dsgdfgd g");
         File f1 = new File(dirname);
         if (f1.isDirectory()) {
             System.out.println("目录 " + dirname);
--- Employee.java
+++ Employee.java
@@ -1,4 +1,4 @@
-package example;
+package example.testForJava;
 
 public class Employee implements java.io.Serializable
 {
